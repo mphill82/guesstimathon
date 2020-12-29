05dcf9895a1d2b4517bdf37ad3f74d863bc22285
Mitch Phillips <mphillips2013@gmail.com>
2020-12-29 17:02:22 -0500

    invalid, incorrect, and correct proper display

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 
 var displayTimeRemaining;
 var tries = 18;
+var correct = new Array();  //this array will store 0 for each incorrect question and floor(b/a) for each correct question.  it will unpdate each time an answer is submitted.
 var rawscore = 10;
 var finalscore = 81920;
+
 
 var questionset = [["How many steps to the top of the Eiffel tower?", 674],
 ["How many gold medals were given at the 2016 Summer Olympic Games?", 307],
@@ -20,6 +22,7 @@ $(function () {
     });
     //create a card for each question in the questionset array and place them in the gameboard
     for (i = 0; i < questionset.length; i++) {
+        correct.push(0); //sets default value to 0 for incorrect for each question.
         htmltext = "<div class='col'>\
                         <div class='card h-100'>\
                             <div class='card-header'>\
@@ -46,7 +49,7 @@ $(function () {
                     </div>";
         $("#gameboard").append(htmltext);
     }
-
+    console.log(correct);
 });
 
 function start() {
@@ -133,19 +136,48 @@ function timer(state) {
 }
 
 function submit(buttonelement) {
-    qn = buttonelement.id.replace("_submit", "");
-    a = parseInt($("#" + qn + "_a").val());
-    b = parseInt($("#" + qn + "_b").val());
+    n = buttonelement.id.replace("_submit", "").replace("q", "");
+    a = parseInt($("#q" + n + "_a").val());
+    b = parseInt($("#q" + n + "_b").val());
+    answer = questionset[n - 1][1];
+
     //check for invalid inputs
-    if ((b <= a) || (a <= 0)) {
-        $("#" + qn + "_status").html("<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='red' class='bi bi-exclamation-circle' viewBox='0 0 16 16'>\
+    if ((b <= a) || (a <= 0) || isNaN(a) || isNaN(b)) {
+        console.log("error");
+        $("#q" + n + "_status").html("Invalid <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='red' class='bi bi-exclamation-circle' viewBox='0 0 16 16'>\
         <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z'/>\
         <path d='M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z'/>\
         </svg>");
-        $("#" + qn + "_a").css("color", "red");
-        $("#" + qn + "_b").css("color", "red");
+        $("#q" + n + "_status").css("color", "red");
+        $("#q" + n + "_a").css("color", "red");
+        $("#q" + n + "_b").css("color", "red");
     }
-
+    //check if the answer is incorrect
+    else if ((answer <= a) || (answer >= b)) {
+        console.log("incorrect");
+        tries--;
+        correct[n - 1] = 0;
+        $("#q" + n + "_status").html("Incorrect <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='black' class='bi bi-x-circle' viewBox='0 0 16 16'>\
+        <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z'/>\
+        <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>\
+      </svg>");
+        $("#q" + n + "_status").css("color", "black");
+        $("#q" + n + "_a").css("color", "black");
+        $("#q" + n + "_b").css("color", "black");
+    }
+    //the answer must be correct
+    else {
+        console.log("correct");
+        tries--;
+        correct[n - 1] = Math.floor(b / a);
+        console.log(correct[n - 1]);
+        $("#q" + n + "_status").html("Correct <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='Green' class='bi bi-check-circle-fill' viewBox='0 0 16 16'>\
+        <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z'/>\
+      </svg>");
+        $("#q" + n + "_status").css("color", "Green");
+        $("#q" + n + "_a").css("color", "black");
+        $("#q" + n + "_b").css("color", "black");
+    }
 
 }
 
