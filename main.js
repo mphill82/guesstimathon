9272a86def005852fdc33f6a0712b55d00582797
Mitch Phillips <mphillips2013@gmail.com>
2020-12-29 15:49:44 -0500

    Accepts answer submits and checks for validity

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
 var displayTimeRemaining;
+var tries = 18;
+var rawscore = 10;
+var finalscore = 81920;
 
 var questionset = [["How many steps to the top of the Eiffel tower?", 674],
 ["How many gold medals were given at the 2016 Summer Olympic Games?", 307],
 ["Evaluate ln(ln(1000000)) rounded to the nearest thousandth.", 2.626],
-["How many toes do Hemingway's cats have?",6]];
+["How many toes do Hemingway's cats have?", 6]];
 
 //run after the HTML loads                    
 $(function () {
@@ -21,22 +24,22 @@ $(function () {
                         <div class='card h-100'>\
                             <div class='card-header'>\
                                 <div class='row'>\
-                                    <div class='col' id='q"+(i+1)+"_title'>Question "+(i+1)+"</div>\
-                                    <div class='col' id='q"+(i+1)+"_status' style='text-align: right'></div>\
+                                    <div class='col' id='q"+ (i + 1) + "_title'>Question " + (i + 1) + "</div>\
+                                    <div class='col' id='q"+ (i + 1) + "_status' style='text-align: right'></div>\
                                 </div>\
                             </div>\
                             <div class='card-body'>\
-                                <p class='card-text' id='q"+(i+1)+"_question'></p>\
-                                <p class='card-text' id='q"+(i+1)+"_answer'></p>\
+                                <p class='card-text' id='q"+ (i + 1) + "_question'></p>\
+                                <p class='card-text' id='q"+ (i + 1) + "_answer' style='color:blue'></p>\
                             </div>\
                             <div class='card-footer'>\
                                 <div class='input-group input-group-sm mb-3'>\
                                     <span class='input-group-text'>(</span>\
-                                    <input type='text' class='form-control' id='q"+(i+1)+"_a' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm' disabled>\
+                                    <input type='text' class='form-control' id='q"+ (i + 1) + "_a' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm' disabled>\
                                     <span class='input-group-text'>,</span>\
-                                    <input  type='text' class='form-control' id='q"+(i+1)+"_b' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm' disabled>\
+                                    <input  type='text' class='form-control' id='q"+ (i + 1) + "_b' aria-label='Sizing example input' aria-describedby='inputGroup-sizing-sm' disabled>\
                                     <span class='input-group-text'>)</span>\
-                                    <button  type='button' class='btn btn-primary btn-sm' id='q"+(i+1)+"_submit' disabled>Submit</button>\
+                                    <button  type='button' class='btn btn-primary btn-sm' id='q"+ (i + 1) + "_submit' onclick= 'submit(this)'disabled>Submit</button>\
                                 </div>\
                             </div>\
                         </div>\
@@ -68,29 +71,29 @@ function updateDisplay(state) {
         $("#finish").removeAttr("disabled");
 
         //display the questions and enable the interval fields and submit buttons
-        for (i=0;i<questionset.length;i++){
-            $("#q"+(i+1)+"_question").text(questionset[i][0]);
-            $("#q"+(i+1)+"_a").removeAttr("disabled");
-            $("#q"+(i+1)+"_b").removeAttr("disabled");
-            $("#q"+(i+1)+"_submit").removeAttr("disabled");
+        for (i = 0; i < questionset.length; i++) {
+            $("#q" + (i + 1) + "_question").text(questionset[i][0]);
+            $("#q" + (i + 1) + "_a").removeAttr("disabled");
+            $("#q" + (i + 1) + "_b").removeAttr("disabled");
+            $("#q" + (i + 1) + "_submit").removeAttr("disabled");
         }
     }
 
     else {
         //disable the finish button, emphasize the final score, deemphasize the other scoreboard items
         $("#finish").attr("disabled", "disabled");
-        $("#time").css("fontWeight","lighter");
-        $("#tries").css("fontWeight","lighter");
-        $("#rawscore").css("fontWeight","lighter");
-        $("#finalscore").css("fontWeight","bolder");
-  
+        $("#time").css("fontWeight", "lighter");
+        $("#tries").css("fontWeight", "lighter");
+        $("#rawscore").css("fontWeight", "lighter");
+        $("#finalscore").css("fontWeight", "bolder");
+
 
         //display the answers and disable the interval fields and submit buttons
-        for (i=0;i<questionset.length;i++){
-            $("#q"+(i+1)+"_answer").text("Answer: "+ questionset[i][1]);
-            $("#q"+(i+1)+"_a").attr("disabled","disabled");
-            $("#q"+(i+1)+"_b").attr("disabled","disabled");
-            $("#q"+(i+1)+"_submit").attr("disabled","disabled");
+        for (i = 0; i < questionset.length; i++) {
+            $("#q" + (i + 1) + "_answer").text("Answer: " + questionset[i][1]);
+            $("#q" + (i + 1) + "_a").attr("disabled", "disabled");
+            $("#q" + (i + 1) + "_b").attr("disabled", "disabled");
+            $("#q" + (i + 1) + "_submit").attr("disabled", "disabled");
         }
 
     }
@@ -129,7 +132,20 @@ function timer(state) {
     }
 }
 
-function submit(n) {
+function submit(buttonelement) {
+    qn = buttonelement.id.replace("_submit", "");
+    a = parseInt($("#" + qn + "_a").val());
+    b = parseInt($("#" + qn + "_b").val());
+    //check for invalid inputs
+    if ((b <= a) || (a <= 0)) {
+        $("#" + qn + "_status").html("<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='red' class='bi bi-exclamation-circle' viewBox='0 0 16 16'>\
+        <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z'/>\
+        <path d='M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z'/>\
+        </svg>");
+        $("#" + qn + "_a").css("color", "red");
+        $("#" + qn + "_b").css("color", "red");
+    }
+
 
 }
 
